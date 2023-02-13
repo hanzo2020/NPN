fd9cde358e9b8404cfb7f9bfff87fa5e67e9d091
“陈邦” <9361644+hanzo1999@user.noreply.gitee.com>
2023-02-13 10:17:01 +0800

    2023.2.13

---
+++ NPNMIX.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from Flatten import Flatten
+from utils import utils
+import numpy as np
+from mlpMixer import ProjectionMixer
+
+
+class NPNMIX(nn.Module):
+    def __init__(self, device, class_num, batch_size):
+        super(NPNMIX, self).__init__()
+        self.device = device
+        self.batch_size = batch_size
+        self.class_num = class_num
+        self.obj_dimension = 64
+        self.concept_embeddings = torch.nn.Embedding(self.class_num, self.obj_dimension)
+        self.concept = torch.arange(0, self.class_num, dtype=int)
+        self.true = torch.nn.Parameter(utils.numpy_to_torch(
+            np.random.uniform(0, 1, size=[1, self.obj_dimension]).astype(np.float32)), requires_grad=False)
+        self.sim_scale = 10
+        self.net = nn.Sequential(#224X224
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Flatten(),  # 1568
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.2),
+            nn.Linear(4096, 2048),
+            nn.ReLU(True),
+            nn.Dropout(p=0.2),
+            nn.Linear(2048, self.obj_dimension),
+        )
+        self.belong_to = ProjectionMixer(1, self.obj_dimension)
+
+
+    def forward(self, x):
+        pre = self.predict(x)
+        return pre
+
+    def similarity(self, vector1, vector2, sigmoid=True):
+        result = F.cosine_similarity(vector1, vector2, dim=-1)
+        result = result * self.sim_scale
+        if sigmoid:
+            return result.sigmoid()
+        return result
+
+    def predict(self, x):
+        d = self.net(x)
+        concepts = self.concept_embeddings(self.concept.to(self.device))
+        concepts1 = concepts.expand(d.shape[0], self.class_num, self.obj_dimension)
+        d1 = d.expand(self.class_num, d.shape[0], d.shape[1])
+        d2 = d1.transpose(0, 1)#4X64X64 to 64X4X64
+        d3 = d2.reshape(self.class_num*d.shape[0], self.obj_dimension)
+        concepts2 = concepts1.reshape(self.class_num*d.shape[0], self.obj_dimension)
+        #concepts2 = torch.cat((d2, concepts1), dim=-1)
+        concepts3 = self.belong_to(d3, concepts2)
+        concepts3 = concepts3.reshape(d.shape[0], self.class_num, self.obj_dimension)
+        pre = self.similarity(concepts3, self.true, sigmoid=True)
+        return pre
+
+
+
+
